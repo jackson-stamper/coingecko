0f35bf2d98dec73a7c2ac75601064248ab7b5327
danielbohnemann <dannybohnemann@yahoo.com>
2020-12-03 20:48:50 -0600

    first connection

--- db.js
+++ db.js
@@ -1,1 +1,11 @@
 const Pool = require("pg").Pool;
+
+const pool = new Pool ({
+    user: "postgres",
+    password: "pgadmin4",
+    host: "coins.cqsjdcazj1ea.us-east-1.rds.amazonaws.com",
+    port: 5432,
+    database: "coin"
+});
+
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//routes
+
+//get all todos
+app.get("/ethereum", async(req,res) => {
+    try {
+        const allData = await pool.query("SELECT * FROM ethereum");
+        res.json(allData.rows);
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+app.listen(5000, () => {
+    console.log("server has started on port 5000");
+});
