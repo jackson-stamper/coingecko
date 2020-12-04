1cd9a99f6d19d0f745d07603fd0712c25412375d
jackson-stamper <jacksonstamper32@gmail.com>
2020-12-03 23:02:13 -0600

    added all coins in tracking

--- index.js
+++ index.js
@@ -28,6 +28,69 @@ app.get("/chainlink", async(req,res) => {
     }
 })
 
+app.get("/ethlend", async(req,res) => {
+    try {
+        const allData = await pool.query("SELECT * FROM ethlend");
+        res.json(allData.rows);
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+app.get("/havven", async(req,res) => {
+    try {
+        const allData = await pool.query("SELECT * FROM havven");
+        res.json(allData.rows);
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+app.get("/nxm", async(req,res) => {
+    try {
+        const allData = await pool.query("SELECT * FROM nxm");
+        res.json(allData.rows);
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+app.get("/rarible", async(req,res) => {
+    try {
+        const allData = await pool.query("SELECT * FROM rarible");
+        res.json(allData.rows);
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+app.get("/uma", async(req,res) => {
+    try {
+        const allData = await pool.query("SELECT * FROM uma");
+        res.json(allData.rows);
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+app.get("/uniswap", async(req,res) => {
+    try {
+        const allData = await pool.query("SELECT * FROM uniswap");
+        res.json(allData.rows);
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+app.get("/yearnfinance", async(req,res) => {
+    try {
+        const allData = await pool.query("SELECT * FROM yearnfinance");
+        res.json(allData.rows);
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
 app.listen(5000, () => {
     console.log("server has started on port 5000");
 });
